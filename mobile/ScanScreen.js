3e9710793425f5f4010e7aa73dcf6bdcb399eef7
Ines <ines_gbadamassi@outlook.fr>
2024-04-29 22:28:15 +0200

    Create ScanScreen.js for camera to take save and retake pictures

--- mobile/ScanScreen.js
+++ mobile/ScanScreen.js
@@ -1,10 +1,80 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import * as MediaLilbrary from 'expo-media-library';
+import Button from './src/components/Button';
+
 
 const ScanScreen = () => {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);  
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+        MediaLilbrary.requestPermissionsAsync();
+        const cameraStatus = await Camera.requestCameraPermissionsAsync();
+        setHasCameraPermission(cameraStatus.status === 'granted');
+    })();
+  }, [])
+
+  const takePicture = async () => {
+    if(cameraRef) {
+        try{
+            const data = await cameraRef.current.takePictureAsync();
+            console.log(data);
+            setImage(data.uri);
+        } catch(e) {
+            console.log(e);
+        }
+    }
+  }
+
+  const saveImage = async () => {
+    if(image) {
+        try{
+            await MediaLilbrary.createAssetAsync(image);
+            alert('Picture save!')
+            setImage(null);
+        } catch(e) {
+            console.log(e)
+        }
+    }
+  }
+
+  if(hasCameraPermission === false){
+    return <Text>No access to camera</Text>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Scan Page</Text>
+        {!image ?
+        <Camera
+            style={styles.camera}
+            type={type}
+            flashMode={flash}
+            ref={cameraRef}
+        >
+        </Camera>
+        :
+        <Image source={{uri: image}} style={styles.camera}/>
+        }
+        <View>
+            {image ?
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                paddingHorizontal: 50,
+            }}>
+                <Button title={"Re-take"} icon="retweet" onPress={() => setImage(null)} />
+                <Button title={"Save"} icon="check" onPress={saveImage} />
+            </View>
+            :
+            <Button title={'Take a picture'} icon="camera" onPress={takePicture} />
+            }
+        </View>
     </View>
   );
 };
@@ -12,9 +82,14 @@ const ScanScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgoundColor: '#000',
     justifyContent: 'center',
-    alignItems: 'center',
+    paddingBottom: 20,
   },
+  camera: {
+    flex: 1,
+    borderRadius: 20,
+  }
 });
 
 export default ScanScreen;
