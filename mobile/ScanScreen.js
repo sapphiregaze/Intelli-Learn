e426d19b316b8284c258c93b7b3e52d6056bc7a3
Ines <ines_gbadamassi@outlook.fr>
2024-04-29 22:36:09 +0200

    Finish to set up camera screen

--- mobile/ScanScreen.js
+++ mobile/ScanScreen.js
@@ -57,6 +57,23 @@ const ScanScreen = () => {
             flashMode={flash}
             ref={cameraRef}
         >
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                paddingHorizontal: 30,
+            }}>
+                <Button icon={'retweet'} onPress={ () => {
+                    setType(type === CameraType.back ? CameraType.front : CameraType.back)
+                }} />
+                <Button icon={'flash'} 
+                    color={flash === Camera.Constants.FlashMode.off ? 'gray' : '#f1f1f1'}
+                    onPress={ () => {
+                        setFlash(flash === Camera.Constants.FlashMode.off 
+                        ? Camera.Constants.FlashMode.on 
+                        : Camera.Constants.FlashMode.off
+                        )
+                    }} />
+            </View>
         </Camera>
         :
         <Image source={{uri: image}} style={styles.camera}/>
