6431d02ccf4d05ac6600037843a2822fecd191da
Ines <ines_gbadamassi@outlook.fr>
2024-04-30 18:24:16 +0200

    Finish DataScreen.js UI

--- mobile/DataScreen.js
+++ mobile/DataScreen.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import item1Image from './assets/item1.png';
+import item2Image from './assets/item2.png';
+import item3Image from './assets/item3.png';
+
+const items = [
+  { key: 'item1', title: 'Learning Progress', hours: '12.00 hours', image: item1Image },
+  { key: 'item2', title: 'Daily Average Study Time', hours: '3.00 hours', image: item2Image },
+  { key: 'item3', title: 'Completed Educational Goals', hours: '15.00 hours', image: item3Image }
+];
 
 export default function DataScreen() {
   return (
@@ -8,7 +17,7 @@ export default function DataScreen() {
       <Text style={styles.subHeader}>This Week</Text>
       <TouchableOpacity style={styles.button}>
         <View style={styles.analysisContainer}>
-          <Text style={styles.buttonText}>study time analysis</Text>
+          <Text style={styles.buttonText}>Study time analysis</Text>
           <Text style={styles.hours}>00.00 hours</Text>
         </View>
         <View style={styles.barContainer}>
@@ -24,6 +33,18 @@ export default function DataScreen() {
           ))}
         </View>
       </TouchableOpacity>
+      <Text style={styles.performanceHeader}>My Performance</Text>
+      {items.map((item, index) => (
+        <TouchableOpacity key={index} style={styles.performanceButton}>
+          <View style={styles.iconContainer}>
+            <Image source={item.image} style={styles.icon} />
+          </View>
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoTitle}>{item.title}</Text>
+            <Text style={styles.infoHours}>{item.hours}</Text>
+          </View>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
@@ -33,25 +54,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
-    paddingTop: 30,
+    paddingTop: 20,
     backgroundColor: '#F3F6FA',
   },
   header: {
     fontSize: 24,
     fontWeight: 'bold',
     color: '#5F7AF9',
-    marginBottom: 14,
+    marginBottom: 10,
   },
   subHeader: {
-    marginTop: 10,
     fontSize: 18,
     fontWeight: 'bold',
     alignSelf: 'flex-start',
     marginLeft: 20,
     color: '#000',
+    marginBottom: 10,
   },
   button: {
-    marginTop: 10,
+    marginTop: 5,
     backgroundColor: '#FFFFFF',
     padding: 20,
     borderRadius: 10,
@@ -94,7 +115,7 @@ const styles = StyleSheet.create({
   },
   innerBar: {
     width: 20,
-    height: 50, 
+    height: 50,
     backgroundColor: '#5F7AF9',
     borderRadius: 5,
   },
@@ -107,5 +128,53 @@ const styles = StyleSheet.create({
   dayText: {
     fontSize: 12,
     color: '#000',
+  },
+  performanceHeader: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 20,
+    color: '#000',
+    marginLeft: -190,    
+  },
+  performanceButton: {
+    flexDirection: 'row',
+    marginTop: 10,
+    backgroundColor: '#FFFFFF',
+    padding: 15,
+    borderRadius: 15,
+    width: '90%',
+    alignItems: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  iconContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#5F7AF9',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 30,
+    height: 30,
+  },
+  infoContainer: {
+    marginLeft: 20,
+  },
+  infoTitle: {
+    fontSize: 16,
+    color: '#000',
+  },
+  infoHours: {
+    fontSize: 14,
+    color: '#000',
+    fontWeight: 'bold',
   },
 });
