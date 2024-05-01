a7480963eed1d9883844d8e532119458bc75a0b4
Ines <ines_gbadamassi@outlook.fr>
2024-05-01 11:47:02 +0200

    Few change

--- mobile/ProfileScreen.js
+++ mobile/ProfileScreen.js
@@ -148,7 +148,7 @@ const styles = StyleSheet.create({
     width: 28,
     height: 28,
     borderRadius: 14,
-    backgroundColor: '#5F7AF9',
+    backgroundColor: '#000000',
   },
   profileName: {
     marginTop: 12,
